117c922db4c7a2f4516dc1dbda441869d9d88b4e
HoenlingerC <e1326676@student.tuwien.ac.at>
2024-04-03 22:21:52 +0200

    Update Test_function
    Test function:
    calculate_active_and_reactive_power_from_apparent_power_and_power_factor

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,7 @@
 """This file tests the functions in functions.py"""
 
 from functions import add
+from functions import calculate_active_and_reactive_power_from_apparent_power_and_power_factor
 
 
 def test_add():
@@ -8,3 +9,10 @@ def test_add():
     assert add(0, 3) == 3
     assert add(3, -2) == 1
     assert add(200, 50) == 250
+
+
+def test_calculate_active_and_reactive_power_from_apparent_power_and_power_factor():
+    assert calculate_active_and_reactive_power_from_apparent_power_and_power_factor(3, 1)[0] == 3
+    assert calculate_active_and_reactive_power_from_apparent_power_and_power_factor(3, 1)[1] == 0
+    assert calculate_active_and_reactive_power_from_apparent_power_and_power_factor(3, 0.9)[0] == 2.7
+    assert calculate_active_and_reactive_power_from_apparent_power_and_power_factor(3, 0.9)[1] == 1.31
