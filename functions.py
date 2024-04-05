910c61a83d9b5fa60fe1f0862bea67f8a1c021e4
HoenlingerC <e1326676@student.tuwien.ac.at>
2024-04-05 21:48:59 +0200

    Update function
    Add range control to function calculate_active_and_reactive_power_from_apparent_power_and_power_factor(s, cos_phi)

--- functions.py
+++ functions.py
@@ -10,6 +10,9 @@ def add(a, b):
 
 def calculate_active_and_reactive_power_from_apparent_power_and_power_factor(s, cos_phi):
     """Calculate active and reactive power from apparent power and power factor cos(phi)"""
+    while(abs(cos_phi) > 1):  #As long as the power factor is greater than one, ask for a new input
+        cos_phi = float(input(
+            'The power factor is greater than one. The Power Factor has to be between -1 and 1. Therefore, please enter a number between -1 and 1:'))
     p = round(s * cos_phi, 2)
     q = round(s * m.sin(m.acos(cos_phi)), 2)
 
